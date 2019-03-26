96b10d8098d42317c0ad189995b6ba561fe615bb
Komalsk06 <komalraje.angular@gmail.com>
2019-03-26 19:38:22 +0530

    extra line remove

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,9 +6,7 @@ import { Component, OnInit } from '@angular/core';
     <section class="hero is-info is-fullheight is-bold">
     <div class="hero-body">
     <div class="container">
-
       <h1 class="title">Home Page!</h1>
-
     </div>
     </div>
     </section>
